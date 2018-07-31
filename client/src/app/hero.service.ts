e2fbe31aafa3d001afba09c085a3d4f5b7c4d5a7
Meshpy <pgitner@gmail.com>
2018-07-31 23:02:00 +0200

    Add hero service which is specified for fetching or saving data

---
+++ client/src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/internal/Observable";
+import {Hero} from "./hero";
+import {HEROES} from "./mock-heroes";
+import {of} from "rxjs/internal/observable/of";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
