1ee1e164ea8dd16bd5120bb833cc45386c357b58
Meshpy <209317@edu.p.lodz.pl>
2018-08-01 16:32:56 +0200

    Add getting hero details by concrete route

--- client/src/app/hero-detail/hero-detail.component.ts
+++ client/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from "../hero";
+import {ActivatedRoute} from "@angular/router";
+import {HeroService} from "../hero.service";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,9 +14,26 @@ export class HeroDetailComponent implements OnInit {
   // external HeroesComponent will bind to this property, that's why we use @Input decorator
   @Input() hero: Hero;
 
-  constructor() { }
+  // ActivatedRoute holds information about the route to this instance of HeroDetailComponent
+  // HeroService gets hero data from the remote server
+  // Location is an Angular service for interacting with the browser
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
