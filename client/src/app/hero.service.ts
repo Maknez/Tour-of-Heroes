49ccb2b99219c3ecac4534055ab67762edfe29d1
Meshpy <209317@edu.p.lodz.pl>
2018-08-02 18:01:56 +0200

    Change hero service on front for http requests

--- client/src/app/hero.service.ts
+++ client/src/app/hero.service.ts
@@ -1,24 +1,71 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs/internal/Observable";
 import {Hero} from "./hero";
-import {HEROES} from "./mock-heroes";
 import {of} from "rxjs/internal/observable/of";
 import {MessageService} from "./message.service";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, tap} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private basicUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+
+    return this.http.get<Hero[]>(`${this.basicUrl}/heroes`)
+      .pipe(
+        tap(heroes => this.log(`Fetched heroes`)),
+        catchError(this.handleError(`getHeroes`, []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+
+    return this.http.get<Hero>(`${this.basicUrl}/heroes/details/${id}`)
+      .pipe(
+        tap(_ => this.log(`Fetched heroes id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.basicUrl}/heroes`, hero, httpOptions)
+      .pipe(
+        tap((hero: Hero) => this.log(`Added hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+
+    return this.http.delete<Hero>(`${this.basicUrl}/heroes/${id}`, httpOptions)
+      .pipe(
+        tap(_ => this.log(`Deleted hero=${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
+      );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
+
+  private log(message: string) {
+    this.messageService.add(`MessageService: ${message}`);
   }
 }
--- toh-server/src/main/java/com/meshpy/rest/HeroRestController.java
+++ toh-server/src/main/java/com/meshpy/rest/HeroRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(allowedHeaders = "Access-Control-Allow-Headers")
 @RestController
 @RequestMapping("/api")
 public class HeroRestController {
