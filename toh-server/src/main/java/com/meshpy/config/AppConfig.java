34bd826481279a29b2b31710af1140d7bc715591
Meshpy <209317@edu.p.lodz.pl>
2018-08-02 14:58:00 +0200

    Add session factory, transaction manager and get hibernate properties methods

--- toh-server/src/main/java/com/meshpy/config/AppConfig.java
+++ toh-server/src/main/java/com/meshpy/config/AppConfig.java
@@ -1,17 +1,21 @@
 package com.meshpy.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 @Configuration
@@ -59,11 +63,47 @@ public class AppConfig {
         return dataSource;
     }
 
+    private Properties getHibernateProperties() {
+
+        // set hibernate properties
+        Properties properties = new Properties();
+
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+
+        return properties;
+    }
+
     // need a helper method
     // read environment property and convert to int
     private int getIntProperty(String propertyName) {
         String propertyValue = environment.getProperty(propertyName);
         int intPropertyValue = Integer.parseInt(propertyValue);
         return intPropertyValue;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+
+        // create session factory
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        // set the properties
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+        // setup transaction manager based on section factory
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+
+        return transactionManager;
     }
 }
